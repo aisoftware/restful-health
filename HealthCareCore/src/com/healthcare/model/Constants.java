2a4d0798787dda5ffeccb85c5c0ffcf26b40309c
twilson650 <twilson650@gmail.com>
2012-10-17 02:41:57 +0000

    Filled out skeleton model with person, patient and lab report

--- HealthCareCore/src/com/healthcare/model/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.healthcare.model;
-
-public class Constants {
-	public static String PATIENT = "patient";
-	public static String LOG_IN_INFO = "loginInfo";
-	public static String PERSON = "person";
-
-}
