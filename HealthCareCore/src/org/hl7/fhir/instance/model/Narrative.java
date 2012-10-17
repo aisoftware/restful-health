8222978649107f0d07c4f09547749bc45899b5f0
twilson650 <twilson650@gmail.com>
2012-10-17 02:37:02 +0000

    Filled out skeleton model with person, patient and lab report

---
+++ HealthCareCore/src/org/hl7/fhir/instance/model/Narrative.java
@@ -0,0 +1,206 @@
+package org.hl7.fhir.instance.model;
+
+/*
+  Copyright (c) 2011-2012, HL7, Inc.
+  All rights reserved.
+  
+  Redistribution and use in source and binary forms, with or without modification, 
+  are permitted provided that the following conditions are met:
+  
+   * Redistributions of source code must retain the above copyright notice, this 
+     list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright notice, 
+     this list of conditions and the following disclaimer in the documentation 
+     and/or other materials provided with the distribution.
+   * Neither the name of HL7 nor the names of its contributors may be used to 
+     endorse or promote products derived from this software without specific 
+     prior written permission.
+  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+  POSSIBILITY OF SUCH DAMAGE.
+  
+*/
+
+// Generated on Wed, Oct 3, 2012 23:31+1000 for FHIR v0.06
+
+import java.util.*;
+
+import org.hl7.fhir.utilities.xhtml.*;
+
+/**
+ * A human readable formatted text, including images
+ */
+public class Narrative extends Element {
+
+    public enum NarrativeStatus {
+        generated, // The contents of the narrative are entirely generated from the structured data in the resource.
+        extensions, // The contents of the narrative are entirely generated from the structured data in the resource and some of the content is generated from extensions
+        additional; // The contents of the narrative contain additional information not found in the structured data
+        public static NarrativeStatus fromCode(String codeString) throws Exception {
+            if (codeString == null || "".equals(codeString))
+                return null;
+        if ("generated".equals(codeString))
+          return generated;
+        if ("extensions".equals(codeString))
+          return extensions;
+        if ("additional".equals(codeString))
+          return additional;
+        throw new Exception("Unknown NarrativeStatus code '"+codeString+"'");
+        }
+        public String toCode() {
+          switch (this) {
+            case generated: return "generated";
+            case extensions: return "extensions";
+            case additional: return "additional";
+            default: return "?";
+          }
+        }
+    }
+
+    public enum NarrativeMapSource {
+        text, // The text is the original data
+        data; // The data is the original data
+        public static NarrativeMapSource fromCode(String codeString) throws Exception {
+            if (codeString == null || "".equals(codeString))
+                return null;
+        if ("text".equals(codeString))
+          return text;
+        if ("data".equals(codeString))
+          return data;
+        throw new Exception("Unknown NarrativeMapSource code '"+codeString+"'");
+        }
+        public String toCode() {
+          switch (this) {
+            case text: return "text";
+            case data: return "data";
+            default: return "?";
+          }
+        }
+    }
+
+    public class Image extends Element {
+        /**
+         * Mime type of image
+         */
+        private String mimeType;
+
+        /**
+         * base64 image data
+         */
+        private byte[] content;
+
+        public String getMimeType() { 
+          return this.mimeType;
+        }
+
+        public void setMimeType(String value) { 
+          this.mimeType = value;
+        }
+
+        public byte[] getContent() { 
+          return this.content;
+        }
+
+        public void setContent(byte[] value) { 
+          this.content = value;
+        }
+
+    }
+
+    public class Map extends Element {
+        /**
+         * The narrative end of the mapping
+         */
+        private String text;
+
+        /**
+         * The resource element end of the mapping
+         */
+        private String data;
+
+        /**
+         * Which end of the mapping is the source
+         */
+        private NarrativeMapSource source;
+
+        public String getText() { 
+          return this.text;
+        }
+
+        public void setText(String value) { 
+          this.text = value;
+        }
+
+        public String getData() { 
+          return this.data;
+        }
+
+        public void setData(String value) { 
+          this.data = value;
+        }
+
+        public NarrativeMapSource getSource() { 
+          return this.source;
+        }
+
+        public void setSource(NarrativeMapSource value) { 
+          this.source = value;
+        }
+
+    }
+
+    /**
+     * The status of the narrative - whether it's entirely generated (from just the defined data or the extensions too), or whether a human authored it and it may contain additional data
+     */
+    private NarrativeStatus status;
+
+    /**
+     * The actual narrative content, a stripped down version of XHTML
+     */
+    private XhtmlNode div;
+
+    /**
+     * An image referred to directly in the xhtml
+     */
+    private List<Image> image = new ArrayList<Image>();
+
+    /**
+     * A map from the narrative contents to the resource elements - an assertion that the text describes the some content as the data item describes
+     */
+    private List<Map> map = new ArrayList<Map>();
+
+    public NarrativeStatus getStatus() { 
+      return this.status;
+    }
+
+    public void setStatus(NarrativeStatus value) { 
+      this.status = value;
+    }
+
+    public XhtmlNode getDiv() { 
+      return this.div;
+    }
+
+    public void setDiv(XhtmlNode value) { 
+      this.div = value;
+    }
+
+    public List<Image> getImage() { 
+      return this.image;
+    }
+
+    public List<Map> getMap() { 
+      return this.map;
+    }
+
+
+}
+
---
+++ HealthCareCore/src/org/hl7/fhir/instance/model/Organization.java
@@ -0,0 +1,250 @@
+package org.hl7.fhir.instance.model;
+
+import java.util.*;
+
+/**
+ * For any company/corporation/institution/government department that has relevance to the care process
+ */
+public class Organization {
+
+    public class Name {
+        /**
+         * The actual name of the organization
+         */
+        private String value;
+
+        /**
+         * The period that this name was in use by the organization
+         */
+        private Period period;
+
+        public String getValue() { 
+          return this.value;
+        }
+
+        public void setValue(String value) { 
+          this.value = value;
+        }
+
+        public Period getPeriod() { 
+          return this.period;
+        }
+
+        public void setPeriod(Period value) { 
+          this.period = value;
+        }
+
+    }
+
+    public class Accreditation {
+        /**
+         * The identifier of the accreditation
+         */
+        private Identifier identifier;
+
+        /**
+         * The type of the accreditation
+         */
+        private CodeableConcept code;
+
+        /**
+         * The organization that confered/confers the accreditation
+         */
+        private ResourceReference institution;
+
+        /**
+         * The period for which the accreditation is held
+         */
+        private Period period;
+
+        public Identifier getIdentifier() { 
+          return this.identifier;
+        }
+
+        public void setIdentifier(Identifier value) { 
+          this.identifier = value;
+        }
+
+        public CodeableConcept getCode() { 
+          return this.code;
+        }
+
+        public void setCode(CodeableConcept value) { 
+          this.code = value;
+        }
+
+        public ResourceReference getInstitution() { 
+          return this.institution;
+        }
+
+        public void setInstitution(ResourceReference value) { 
+          this.institution = value;
+        }
+
+        public Period getPeriod() { 
+          return this.period;
+        }
+
+        public void setPeriod(Period value) { 
+          this.period = value;
+        }
+
+    }
+
+    public class RelatedOrganization {
+        /**
+         * Identifier the related organization - may be a full link to an Organization resource, or some other kind of identifier
+         */
+        private HumanId identifier;
+
+        /**
+         * Code that specifies how this organization is related to the subject. A code is required.
+         */
+        private CodeableConcept code;
+
+        /**
+         * A name should be specified for the related organization
+         */
+        private String name;
+
+        /**
+         * Postal addresses may be provided for the related organization
+         */
+        private List<Address> address = new ArrayList<Address>();
+
+        /**
+         * Contact details (phone, email etc) may be provided for the related organization
+         */
+        private List<Contact> contact = new ArrayList<Contact>();
+
+        /**
+         * The period during which the organizations were related in this fashion
+         */
+        private Period period;
+
+        public HumanId getIdentifier() { 
+          return this.identifier;
+        }
+
+        public void setIdentifier(HumanId value) { 
+          this.identifier = value;
+        }
+
+        public CodeableConcept getCode() { 
+          return this.code;
+        }
+
+        public void setCode(CodeableConcept value) { 
+          this.code = value;
+        }
+
+        public String getName() { 
+          return this.name;
+        }
+
+        public void setName(String value) { 
+          this.name = value;
+        }
+
+        public List<Address> getAddress() { 
+          return this.address;
+        }
+
+        public List<Contact> getContact() { 
+          return this.contact;
+        }
+
+        public Period getPeriod() { 
+          return this.period;
+        }
+
+        public void setPeriod(Period value) { 
+          this.period = value;
+        }
+
+    }
+
+    /**
+     * Identifier for the organization that is used to identify the organization across multiple disparate systems
+     */
+    private List<HumanId> identifier = new ArrayList<HumanId>();
+
+    /**
+     * A name associated with the organization
+     */
+    private List<Name> name = new ArrayList<Name>();
+
+    /**
+     * An address for the organization
+     */
+    private List<Address> address = new ArrayList<Address>();
+
+    /**
+     * A contact detail for the organization
+     */
+    private List<Contact> contact = new ArrayList<Contact>();
+
+    /**
+     * The kind of organization that this is
+     */
+    private CodeableConcept code;
+
+    /**
+     * The industry that this organization is involved in
+     */
+    private CodeableConcept industryCode;
+
+    /**
+     * The qualifications/certifications an organisation has, including format educational achievements, accreditations, and current certifications. All these qualifications may be used to determine what roles the organisation may play in a healthcare environment
+     */
+    private List<Accreditation> accreditation = new ArrayList<Accreditation>();
+
+    /**
+     * Other organizations who are related to this person. The relationship might be one of several types: sub- or super- orgnizations (i.e. ward in a hospital, owning corporation of a hospital) or partner organizations (i.e. the operating corporation for a hospital)
+     */
+    private List<RelatedOrganization> relatedOrganization = new ArrayList<RelatedOrganization>();
+
+    public List<HumanId> getIdentifier() { 
+      return this.identifier;
+    }
+
+    public List<Name> getName() { 
+      return this.name;
+    }
+
+    public List<Address> getAddress() { 
+      return this.address;
+    }
+
+    public List<Contact> getContact() { 
+      return this.contact;
+    }
+
+    public CodeableConcept getCode() { 
+      return this.code;
+    }
+
+    public void setCode(CodeableConcept value) { 
+      this.code = value;
+    }
+
+    public CodeableConcept getIndustryCode() { 
+      return this.industryCode;
+    }
+
+    public void setIndustryCode(CodeableConcept value) { 
+      this.industryCode = value;
+    }
+
+    public List<Accreditation> getAccreditation() { 
+      return this.accreditation;
+    }
+
+    public List<RelatedOrganization> getRelatedOrganization() { 
+      return this.relatedOrganization;
+    }
+
+
+}
+
+
