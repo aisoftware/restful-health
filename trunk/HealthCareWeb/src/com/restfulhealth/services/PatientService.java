fa74d9a9c02dd54ef1db745a4eec32349c607dc3
twilson650@gmail.com <twilson650@gmail.com@a83422eb-89ad-1aeb-fa35-0d20b62dd204>
2012-10-19 23:43:52 +0000

    Added lab report API

--- trunk/HealthCareWeb/src/com/restfulhealth/services/PatientService.java
+++ trunk/HealthCareWeb/src/com/restfulhealth/services/PatientService.java
@@ -1,5 +1,45 @@
 package com.restfulhealth.services;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * Restful services related to the patient object
+ * 
+ * @author Tom Wilson
+ * @author Jing Song
+ *
+ */
 public class PatientService {
+
+	/**
+	 * Return a Patient object, given the identifier
+	 * 
+	 * @param identifier patient identifier
+	 * @return
+	 */
+	@GET
+	@Path("/patient/{identifier}")
+	public Response getPatient(@PathParam("identifier") String identifier) {
+		return Response.status(200)
+				.entity("Would have gotten patient " + identifier).build();
+	}
+	
+	/**
+	 * Returns a summary list of labs, as a collection of LabReport objects
+	 * The LabReport objects do not need to be fully populated, only require:
+	 * reportId, reportName, diagnosticTime, conclusion
+	 * 
+	 * @param identifier the patient identifier
+	 * @return a collection of LabReport
+	 */
+	@GET
+	@Path("/patient/{identifier}/labs")
+	public Response getPatientLabs(@PathParam("identifier") String identifier) {
+		return Response.status(200)
+				.entity("Would have gotten labs for patient " + identifier).build();
+	}
 
 }
