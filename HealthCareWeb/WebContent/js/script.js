874416330044fb6689f8304e0ceaaa801ebb8461
mikhail.xu <mikhail.xu@gmail.com>
2012-10-20 05:36:57 +0000

    added datatables, and basic array stuff, heres jings mapping stuff

--- HealthCareWeb/WebContent/js/script.js
+++ HealthCareWeb/WebContent/js/script.js
@@ -1,10 +1,35 @@
 //DOM READY loop
 $(function(){
-	if($('body#login').exists()){
-		$('form .btn').bind('click', function(){
+	//login
+	if($('body#login').exists()) {
+		$('form .btn').bind('click', function() {
+			var data = {};
+			data.username=$('#usrName').val();
+			data.password=$('#usrPassword').val();
 			$.ajax({
-				url:'http://localhost:8080/healthcare/login'
+				url:'http://localhost:8080/healthcare/login',
+				data:data
 			});
 		});
 	}
+	
+	//register
+	if($('body#register').exists()) {
+		$('form .btn').bind('click', function() {
+			var data = {};
+			data.username=$('#usrName').val();
+			data.password=$('#usrPassword').val();
+			$.ajax({
+				url:'http://localhost:8080/healthcare/register',
+				data:data
+			});
+		});
+	}
+	
+	//home
+	if($('body#home').exists()) {
+		
+	}
+	
+	
 });
