cfef54bdc09c55073101e69811b73b938bb7d270
sheeysong@gmail.com <sheeysong@gmail.com@a83422eb-89ad-1aeb-fa35-0d20b62dd204>
2012-10-21 04:44:53 +0000

    checked lab

--- trunk/HealthCareWeb/src/com/restfulhealth/services/LabService.java
+++ trunk/HealthCareWeb/src/com/restfulhealth/services/LabService.java
@@ -1,19 +1,44 @@
 package com.restfulhealth.services;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
 import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
+import net.sf.json.JSON;
+import net.sf.json.xml.XMLSerializer;
+
+import org.apache.commons.io.IOUtils;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+
+@Path("/labReport")
 public class LabService {
-
+	private String dbCollectionName = "labResult"; 
 	
 	public LabService(@Context ServletContext servletContext) {
 		if (ServiceUtil.mongoDBname == null) {
-		ServiceUtil.init(servletContext);
+			ServiceUtil.init(servletContext);
 		}
 	}
 
@@ -23,17 +48,98 @@ public class LabService {
 	 * @return
 	 */
 	@GET
-	@Path("/labReport/{reportId}")
-	public Response getLab(@PathParam("reportId") String reportId) {
-		return Response.status(200)
-				.entity("Would have gotten lab report " + reportId).build();
+	@Path("/{reportId}")
+	public Response getLab(@PathParam("reportId") String reportId) throws Throwable{	
+		String labJSON = "";
+		try{
+			BasicDBObject query = new BasicDBObject();
+			query.put("documentID", reportId);
+			ArrayList<DBObject> obj = ServiceUtil.mongo.query(dbCollectionName, query);
+			if(obj != null && obj.size() >0){
+				DBObject dbo = obj.get(0);
+				Object labObj = dbo.get("labJSON");
+				labJSON = labObj.toString();
+			}
+		}
+		catch(Throwable t){
+			t.printStackTrace();
+			throw t;
+		}
+		return Response.status(200).entity(labJSON).build();
 	}
 	
-	@PUT
-	@Path("/labReport")
-	public Response putLab() {
-		return Response.status(200)
-				.entity("Putting lab report is not yet implemented").build();
+	/* =====================================File upload & Download ============================================================== */
+
+	/**
+	 * This method provides the caller to upload the file with its meta-data through HTTP POST using content-type: multipart/form-data
+	 * @param headers HttpHeaders will carry all meta-data
+	 * @param uploadedInputStream File InputStream
+	 * @param fileDetail Content-Disposition: attachment; filename=xxxxx.file_extension;
+	 * @return HttpResponse with status code or error messages
+	 */
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+			@Context HttpHeaders headers,
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+		String fileName = null;
+		String patientID = null;
+		String documentID = "";
+		try{
+			if(fileDetail != null)
+				fileName = fileDetail.getFileName();
+			else
+				return Response.noContent().status(400).build();
+			
+			MultivaluedMap<String, String> maps = headers.getRequestHeaders();
+			if(maps != null){
+				if(maps.get("pid") != null)
+					patientID = maps.get("pid").get(0);
+			}
+			String labXML = IOUtils.toString(uploadedInputStream);			
+			XMLSerializer xmlSerializer = new XMLSerializer(); 
+			JSON json = xmlSerializer.read(labXML);
+			String jsonResult = json.toString(2);
+			jsonResult = jsonResult.replaceAll("@", "");
+			
+			documentID = UUID.randomUUID().toString();
+			BasicDBObject obj = new BasicDBObject();				
+			obj.put("personID", patientID);
+			obj.put("labJSON", jsonResult);
+			obj.put("documentName", fileName);
+			obj.put("documentID", documentID);
+			
+			ServiceUtil.mongo.put(dbCollectionName, obj) ;
+			
+			return Response.status(200).entity(documentID).build();
+		}
+		catch(Throwable t){
+			t.printStackTrace();
+			return Response.status(500).build();
+		}
 	}
+
+	/**
+	 * This method provides the caller to download the file with its meta-data through HTTP GET using content-type: multipart/form-data
+	 * @param headers HttpHeaders will carry all meta-data of the request on the way in Skyline
+	 * @param fileName The key of the value
+	 * @return binary data of the value in HttpResponse body, and the status cod or error messages in HttpResponse headers
+	 */
+	@GET
+	@Path("/download")
+	@Produces(MediaType.MULTIPART_FORM_DATA)
+	public Response downloadFile(@Context HttpHeaders headers, @PathParam("fileName") String fileName){
+		try{
+			
+			return Response.status(200).build();
+		}
+		catch(Throwable t){
+			t.printStackTrace();
+			return Response.status(500).build();
+		}
+	}
+
 
 }
--- trunk/HealthCareWeb/test/com/restfulhealth/services/test/LabResultTest.java
+++ trunk/HealthCareWeb/test/com/restfulhealth/services/test/LabResultTest.java
@@ -17,8 +17,19 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import com.sun.jersey.multipart.FormDataMultiPart;
+import com.sun.jersey.multipart.file.FileDataBodyPart;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+
 public class LabResultTest {
 
+	private static String uri = "http://localhost:8080/healthcare/service/";
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -41,20 +52,17 @@ public class LabResultTest {
 		BufferedWriter bw = null;
 		try{
 	
-			File file = new File("newLabJSONTest.txt");
+			File file = new File("labSample.txt");
 			fw = new FileWriter(file.getAbsoluteFile());
 			bw = new BufferedWriter(fw);
-			InputStream is = LabResultTest.class.getResourceAsStream("/new-sample.xml");
-				String xml = IOUtils.toString(is);
-				
-				XMLSerializer xmlSerializer = new XMLSerializer(); 
-				JSON json = xmlSerializer.read( xml );
-//				json.write(bw);
-				
-				String jsonResult = json.toString(2);
-				jsonResult = jsonResult.replaceAll("@", "");
-				fw.write(jsonResult);
-				System.out.println( jsonResult);
+			InputStream is = LabResultTest.class.getResourceAsStream("/test-sample.xml");
+			String xml = IOUtils.toString(is);				
+			XMLSerializer xmlSerializer = new XMLSerializer(); 
+			JSON json = xmlSerializer.read( xml );			
+			String jsonResult = json.toString(2);
+			jsonResult = jsonResult.replaceAll("@", "");
+			fw.write(jsonResult);
+			System.out.println( jsonResult);
 		}
 		catch(Throwable t){
 			t.printStackTrace();
@@ -69,6 +77,55 @@ public class LabResultTest {
 			catch(Throwable t){
 				t.printStackTrace();
 			}
+		}
+	}
+	
+	@Test
+	public void labReportUpload() {
+		FormDataMultiPart fdmp = new FormDataMultiPart();
+		String pid = "ba75ec50-dbba-4671-8556-79c668a37c1d"; //for jing song (patient)
+		try {
+			Client client = Client.create();
+			WebResource uploadResource = client.resource(uri);			
+			fdmp.bodyPart(new FileDataBodyPart("file", new File("/Users/JingSong/HealthCare/data/test-sample.xml")));
+			ClientResponse cr = uploadResource.path("labReport/upload").type(MediaType.MULTIPART_FORM_DATA_TYPE).header("pid", pid).post(ClientResponse.class, fdmp);
+			fdmp.close();
+			System.out.println("labReportUpload_status="+cr.getStatus());
+			System.out.println("labReportUpload_documentID="+cr.getEntity(String.class));	
+		} 
+		catch (Throwable t) {
+			t.printStackTrace();
+		}
+		finally{
+			try{
+				fdmp.close();
+			}
+			catch(Throwable t){
+				t.printStackTrace();
+			}
+		}
+	}
+	
+	@Test
+	public void getLabReportByID(){		
+		try {
+			String documentId =  "56a1f4d4-00ee-45e0-942f-2fca28d31636"; //song's reportID
+			Client client = Client.create();
+			WebResource clientResource = client.resource(uri+"labReport/"+documentId);
+			
+			ClientResponse getLabReportResp = clientResource.get(ClientResponse.class);
+			
+			System.out.println("getLabReportResp response :" + getLabReportResp.getStatus());
+			String labJSON =  getLabReportResp.getEntity(String.class);
+			if(labJSON != null)
+				System.out.println("Get labReport by UUID :" + labJSON);
+			else
+				System.out.println("get no patlabient data back");
+		
+			System.out.println(" .... \n");
+		}
+		catch(Throwable t){
+			t.printStackTrace();
 		}
 	}
 
